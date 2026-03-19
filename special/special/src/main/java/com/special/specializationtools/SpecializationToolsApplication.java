577c7a876b763e0efa82ad71f527d0972a6a55f2
Theodora Ferentinou <cs05379@uoi.gr>
2026-03-19 19:41:37 +0200

    piri

---
+++ special/special/src/main/java/com/special/specializationtools/SpecializationToolsApplication.java
@@ -0,0 +1,13 @@
+package com.special.specializationtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecializationToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecializationToolsApplication.class, args);
+	}
+
+}
---
+++ special/special/src/test/java/com/special/specializationtools/SpecializationToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.special.specializationtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecializationToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
