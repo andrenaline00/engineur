3bdc38430f2ed4b7a45dd5fc14305c0fc8ddbeaf
Theodora Ferentinou <cs05379@uoi.gr>
2026-04-14 18:24:46 +0300

    crc prrrrrrrrrrrrrr

--- special/special/src/main/java/com/special/domain/CRCCard.java
+++ special/special/src/main/java/com/special/domain/CRCCard.java
@@ -1,6 +1,57 @@
 package com.special.domain;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="crc_cards")
 public class CRCCard {
 
-	// @Entity
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="class_name",unique=true)
+	private String className;
+	
+	@Column(name="responsibilities")
+	private String responsibilities;
+	
+	@Column(name = "collaborations")
+	private String collaborations;
+	
+	
+	public String getclassname() {
+		return className;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setclassname(String className) {
+		this.className = className;
+	}
+	
+	public String getResponsibilities() { 
+		return responsibilities; 
+	}
+	
+	public void setResponsibilities(String responsibilities) { 
+		this.responsibilities = responsibilities;
+	}
+
+	public String getCollaborations() {
+		return collaborations; 
+	}
+	
+	public void setCollaborations(String collaborations) {
+		this.collaborations = collaborations;
+	}
+
+	
 }
