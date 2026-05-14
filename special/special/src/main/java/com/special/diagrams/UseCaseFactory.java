71d8886a7e6b40372ee4ad913947d0156c434822
Theodora Ferentinou <cs05379@uoi.gr>
2026-05-14 22:30:46 +0300

    klklklkl

--- special/special/src/main/java/com/special/diagrams/UseCaseFactory.java
+++ special/special/src/main/java/com/special/diagrams/UseCaseFactory.java
@@ -1,5 +1,14 @@
 package com.special.diagrams;
 
-public interface UseCaseFactory {
+
+public  UseCaseFactory {
+	
+	public UseCaseDiagramStrategy create(String tool) {
+        return switch (tool.toLowerCase()) {
+            case "plantuml" -> new PlantUmlUseCaseDiagramGenerator();
+            case "nomnoml" -> new NomnomlUseCaseDiagramGenerator();
+            default -> throw new IllegalArgumentException("Unsupported UML tool: " + tool);
+        };
+    }
 
 }
